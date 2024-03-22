2a338f4cc88203cceb1c4374378fd452910db9fe
Legopitstop <1589lego@gmail.com>
2024-03-22 18:06:02 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ComingSoon />
+</template>
+
+<script>
+import ComingSoon from './components/ComingSoon.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ComingSoon
+  }
+}
+</script>
+
+<style>
+:root {
+  background-color: #1f1f1f;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+</style>./components/Temp.vue
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h2>Coming Soon!</h2>
+    <h3 @click="test">Visit old site</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComingSoon',
+  methods: {
+    test: function () {
+      var suffix = '';
+      if (!location.pathname.endsWith('.html')) {suffix = '.html'}
+      location.href = 'https://legopitstop.weebly.com'+location.pathname+suffix;
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2  {
+  font-size: 4em;
+  text-align: center;
+}
+
+h3 {
+  color: #f80;
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.hello {
+  background-color: #181818;
+  padding: 1em;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  border-radius: 1em;
+}
+</style>
